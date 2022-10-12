a8edc204ca075a34e2b764e2900e34a6f1813a5e
Arvind Kanojiya <sonikakanojia543@gmail.com>
2022-10-12 12:55:54 +0530

    sec repo

---
+++ Mathematical_Operator/problem2.js
@@ -0,0 +1,23 @@
+// Modulo will  giving remainder
+let x = 7;
+let  y = 3;
+console.log(x%y)
+
+// Exponentiation it means power
+// squre of number
+let x1 = 4;
+let  y1 = 2;
+console.log(x1**y1)
+
+//squre root of number
+let x2 = 16;
+let  y2 = 0.5;
+console.log(x2**y2)
+console.log(4**y2)
+console.log(25**y2)
+
+//cube root of number
+let x3 = 64;
+let y3 = 0.3;
+console.log(x3**y3)
+
---
+++ Mathematical_Operator/problem3 (1).js
@@ -0,0 +1,22 @@
+// 1) variable name is not start with Number
+
+// let 123name = 34;
+// console.log(123name)
+
+// 2) variable name is not start with symbol accept $ and _
+
+let $name = 34;
+console.log($name);
+
+let _name = 34;
+console.log(_name);
+
+// 3) variable names should be self explanatory
+
+let name = "rahul";
+let father_name = "rahul";
+let last_name = "rahul";
+
+// 4) variable name should not be keyword
+
+// like , for ,while etc.
